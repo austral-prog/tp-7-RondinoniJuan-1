4d0633a0f94bad6b47fca62720e598be8e4b8697
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-10-09 19:00:41 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,42 @@
 def index_of_by_index(word, list, index):
-    return -1
+    for i in range(index, len(list)):
+        if list[i] == word:
+            return i
 
+    return -1
 
 def index_of_empty(list):
-    return -1
+    for i in range(len(list)):
+        if list[i] == "":
+            return i
 
+    return -1
 
 def index_of(word, list):
+    for i in range(len(list)):
+        if list[i] == word:
+            return i
+
     return -1
 
+print(index_of("Black", ["Red", "Green", "White", "Black", "Pink", "Yellow", "Black"]))
 
 def put(word, list):
+    for i in range(len(list)):
+        if list[i] == "":
+            list[i] = word
+            return i
+
     return -1
 
-
 def remove(word, list):
+    count = 0
+
+    for i in range(len(list)):
+        if list[i] == word:
+            list[i] = ""
+            count += 1
+
+    return count
+    
     return -1
